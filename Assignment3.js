4fe337b6ac84d49c210451fce128794632533750
ShrutiGova <shrutigova@gmail.com>
2022-01-25 17:25:33 +0530

    Initial commit

---
+++ Assignment3.js
@@ -0,0 +1,39 @@
+//Assignment 3
+
+//Data Types: Number, String, Boolean
+
+let fristName='Shruti';
+let email='abcd@gmail.com';
+let age='20';
+let address='Shajapur';
+let phone_number=1234567890;
+let gender='Female';
+let isMarried=false;
+console.log(fristName,email,age,address,phone_number,gender,isMarried)
+
+//Output:
+//Shruti
+//abcd@gmail.com
+//20
+//Shajapur
+//1234567890
+//Female
+//false
+
+//Object key()
+const object1 ={
+    a: 'x',
+    b: '2',
+    c: 'false'
+};
+console.log(Object.keys(object1));
+
+//Output:Array["a","b","c"]
+
+//Object value()
+
+console.log(Object.values(object1));
+
+//Outout : Array["x", "2", "false"]
+
+
